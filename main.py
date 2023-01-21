9e7983c9cf2f65d2406bb8383b012350c5d8ea82
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2023-01-21 21:00:34 +0000

    Add preliminary message box frame
    - Add preliminary message box frame
    - Add models.py for basic AI support
    - Update .gitignore

--- main.py
+++ main.py
@@ -1,61 +1,33 @@
 # First things, first. Import the wxPython package.
+from models import OpenAIModel
+import simpleaudio as sa
+import random
+import json
 import wx
 
-class ClippyFrame(wx.Frame):
+with open('./Agent/animations.json') as file:
+    animations = json.loads(file.read())
+
+with open('./Agent/states.json') as file:
+    states = json.loads(file.read())
+
+with open('./Agent/info.json') as file:
+    info = json.loads(file.read())
+
+
+class ClippyTextBox(wx.Frame):
     def __init__(self, *args, **kwargs):
-        super(ClippyFrame, self).__init__(*args, style=wx.FRAME_SHAPED|wx.STAY_ON_TOP|wx.FRAME_NO_TASKBAR|wx.NO_FULL_REPAINT_ON_RESIZE|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.CLIP_CHILDREN|wx.CLOSE_BOX,**kwargs)
+        super(ClippyTextBox, self).__init__(*args, style=wx.FRAME_SHAPED|wx.STAY_ON_TOP|wx.FRAME_NO_TASKBAR|wx.NO_FULL_REPAINT_ON_RESIZE|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.CLIP_CHILDREN|wx.CLOSE_BOX,**kwargs)
+        #self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
+        self.SetBackgroundColour(wx.Colour(255, 255, 225))
 
-        self.CenterOnScreen(wx.BOTH)
+        self.TextCTRL = wx.TextCtrl(self, style=wx.NO_BORDER|wx.TE_PROCESS_ENTER)
 
-        self.currentAnimationFrame = 0
-        self.delta = wx.Point(0,0)
-
-        self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
-
-        self.bitmap = wx.Bitmap('./bmp/0000.bmp')
-
-        if wx.Platform == "__WXGTK__":
-            self.Bind(wx.EVT_WINDOW_CREATE, self.SetWindowShape)
-        else:
-            self.SetWindowShape()
-
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
-        self.Bind(wx.EVT_MOTION, self.OnMouseMove)
-        self.timer = wx.Timer(self)
-        self.timer.Start(100)
-        self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
-
-    def OnTimer(self, event):
-        if (self.currentAnimationFrame < 901):
-            self.currentAnimationFrame += 1
-        else:
-            self.currentAnimationFrame = 0
-
-        self.Draw()
-
-    def Draw(self):
-        self.Refresh()
-
-    def OnLeftDown(self, event):
-        self.CaptureMouse()
-        x, y = self.ClientToScreen(event.GetPosition())
-        originx, originy = self.GetPosition()
-        dx = x - originx
-        dy = y - originy
-        self.delta = ((dx, dy))
-
-    def OnLeftUp(self, event):
-        if (self.HasCapture()):
-            self.ReleaseMouse()
-
-    def OnMouseMove(self, event):
-        if event.Dragging() and event.LeftIsDown():
-            x, y = self.ClientToScreen(event.GetPosition())
-            fp = (x - self.delta[0], y - self.delta[1])
-            self.SetPosition(fp)
-            #self.widgetsframe.SetPosition(fp)
+        # Make window fancy and transparent
+        ## if wx.Platform == "__WXGTK__":
+        ##     self.Bind(wx.EVT_WINDOW_CREATE, self.SetWindowShape)
+        ## else:
+        ##     self.SetWindowShape()
 
     def SetWindowShape(self):
         self.SetClientSize((self.bitmap.GetWidth(), self.bitmap.GetHeight()))
@@ -63,24 +35,159 @@ class ClippyFrame(wx.Frame):
         self.region = wx.Region(self.bitmap)
         self.hasShape = self.SetShape(self.region)
 
-    def OnPaint(self, event):
-        strNum = str(self.currentAnimationFrame)
+class ClippyFrame(wx.Frame):
+    def __init__(self, *args, animations, states, info, **kwargs):
+        # Create frame
+        super(ClippyFrame, self).__init__(*args, style=wx.FRAME_SHAPED|wx.STAY_ON_TOP|wx.FRAME_NO_TASKBAR|wx.NO_FULL_REPAINT_ON_RESIZE|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.CLIP_CHILDREN|wx.CLOSE_BOX,**kwargs)
+        self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
 
-        while len(strNum) < 4:
-            strNum = '0' + strNum
 
-        self.bitmap = wx.Bitmap('./bmp/' + strNum + '.bmp')
+        # Clippy stuff
+        self.currentState = "Showing"
+        self.currentAnimation = "Show"
+        self.nextState = "IdlingLevel2"
+        self.nextAnimation = "GetArtsy"
+        self.currentAnimationFrame = 0
+        self.bitmap = wx.Bitmap('./Agent/Images/0871.bmp')
+
+        self.animations = animations
+        self.states = states
+        self.info = info
+
+        # Clippy Frame
+        self.msgFrame = ClippyTextBox(None)
+        self.msgFrame.Show(True)
+
+
+
+        # Center the frame on screen (set delta for dragging)
+        self.CenterOnScreen(wx.BOTH)
+        
+        self.MoveMsgFrame()
+        self.delta = wx.Point(0,0)
+
+        # AI Stuff
+        self.AIModel = OpenAIModel()
+
+        # Make window fancy and transparent
+        if wx.Platform == "__WXGTK__":
+            self.Bind(wx.EVT_WINDOW_CREATE, self.SetWindowShape)
+        else:
+            self.SetWindowShape()
+
+        # Bind events
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
+        self.Bind(wx.EVT_MOTION, self.OnMouseMove)
+
+
+
+        # Animation system
+        self.animationTimer = wx.Timer(self)
+        self.animationTimer.StartOnce(10)
+        self.Bind(wx.EVT_TIMER, self.AnimationHandler, self.animationTimer)
+
+    def SetWindowShape(self):
+        self.SetClientSize((self.bitmap.GetWidth(), self.bitmap.GetHeight()))
+
+        self.region = wx.Region(self.bitmap)
+        self.hasShape = self.SetShape(self.region)
+
+    # Handle animation
+    def AnimationHandler(self, event):
+        # Check animation state
+        if (self.currentAnimationFrame >= len(self.animations[self.currentAnimation])):
+            self.currentAnimationFrame = 0
+            if (self.nextAnimation):
+                self.currentAnimation = self.nextAnimation
+                self.nextAnimation = None
+            elif (self.nextState):
+                self.currentState = self.nextState
+                self.nextState = None
+                self.currentAnimation = random.choice(self.states[self.currentState])
+            else:
+                self.currentAnimation = None
+                self.currentState = None
+                return
+
+        # Get image name
+        bitmapName = self.animations[self.currentAnimation][self.currentAnimationFrame]["Image"]
+        soundName = self.animations[self.currentAnimation][self.currentAnimationFrame]["Sound"]
+        if (not bitmapName):
+            self.currentAnimationFrame += 1
+            self.animationTimer.StartOnce(0)
+            return
+
+        self.bitmap = wx.Bitmap('./Agent/Images/' + bitmapName)
         mask = wx.Mask(self.bitmap, wx.Colour(255,0,255,wx.ALPHA_OPAQUE))
         self.bitmap.SetMask(mask)
-        
+
+        # Handle sound stuff
+        if (soundName):
+            #sound = wx.Sound("./Agent/Audio/" + soundName)
+            #sound.Play()
+            #wave = sa.WaveObject.from_wave_file("./Agent/Audio/" + soundName)
+            #play = wave.play()
+            # TODO: Fix sounds
+            pass
+
+        # Redraw window
+        self.Refresh()
+        self.Update()
+
+        # Prepare for next frame
+        self.animationTimer.StartOnce(self.animations[self.currentAnimation][self.currentAnimationFrame]["Duration"])
+        self.currentAnimationFrame += 1
+
+
+
+    # Handle drawing of Clippy
+    def OnPaint(self, event):      
         self.SetWindowShape()
         dc = wx.AutoBufferedPaintDCFactory(self)
         dc.DrawBitmap(self.bitmap, -1, -1, True)
-    
+
+
+
+    # Movement/"Dragging" code
+    def OnLeftDown(self, event):
+        self.CaptureMouse()
+        x, y = self.ClientToScreen(event.GetPosition())
+        self.leftDownOriginX, self.leftDownOriginY = self.GetPosition()
+        dx = x - self.leftDownOriginX
+        dy = y - self.leftDownOriginY
+        self.delta = ((dx, dy))
+
+    def OnLeftUp(self, event):
+        if (self.HasCapture()):
+            self.ReleaseMouse()
+
+        newX, newY = self.GetPosition()
+        if (self.leftDownOriginX == newX and self.leftDownOriginY == newY):
+            ###
+            # TODO: Implement "bubble GUI"
+            ###
+            print("NOT MOVED")
+
+    def OnMouseMove(self, event):
+        if event.Dragging() and event.LeftIsDown():
+            x, y = self.ClientToScreen(event.GetPosition())
+            fp = (x - self.delta[0], y - self.delta[1])
+            self.SetPosition(fp)
+            self.MoveMsgFrame()
+            #self.widgetsframe.SetPosition(fp)
+
+    def MoveMsgFrame(self):
+        msgPosition = self.GetPosition()
+        #self.msgBoxOffset = wx.Point(100, 100)
+        self.msgBoxOffset = self.msgFrame.GetSize()
+        msgPosition -= self.msgBoxOffset
+        self.msgFrame.SetPosition(msgPosition)
 
 class ClippyApp(wx.App):
     def OnInit(self):
-        frame = ClippyFrame(None)
+        frame = ClippyFrame(None, animations=animations, states=states, info=info)
         self.SetTopWindow(frame)
         frame.Show(True)
 
---
+++ models.py
@@ -0,0 +1,58 @@
+import openai
+import json
+
+with open('./secrets.json') as file:
+    secrets = json.loads(file.read())
+
+openai.api_key = secrets["apiKey"]
+
+class OpenAIModel():
+    def __init__(self):
+        self.history = ["Clippy is an AI assistant"]
+        self.model = 'text-davinci-003'
+
+        self.tipModifier = ''
+
+        # Define AI Options
+        self.defaultConfig = {
+            "engine": "text-davinci-003",
+            "temperature": 0.5,
+            "max_tokens": 512,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "useMemory": True
+        }
+        self.config = self.defaultConfig
+
+    def reset(self):
+        self.history = self.history[:1]
+
+    def generateTip(self, prompt):
+        try:
+            completion = openai.Completion.create(engine=self.model, prompt='\n'.join(self.history))
+            response = completion.choices[0].text
+            response = response.strip()
+        except openai.error.RateLimitError as e:
+            response = "[RATELIMITED]\nOur server are overloaded!"
+
+    def prompt(self, prompt):
+        prompt = prompt.strip()
+        self.history.append("User: " + prompt)
+        self.history.append("Response: ")
+
+        while True:
+            try:
+                completion = openai.Completion.create(engine=self.model, prompt='\n'.join(self.history))
+                response = completion.choices[0].text
+                response = response.strip()
+
+                self.history[-1] = "Response: " + response
+                break
+            except openai.error.RateLimitError as e:
+                response = "[RATELIMITED]\nOur server are overloaded! Please wait a bit."
+                break
+            except openai.error.InvalidRequestError as e:
+                self.history = self.history[3:-3]
+
+        return response
